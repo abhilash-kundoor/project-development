fd1c5c3bea3e2b40c62f7b4a1bf43fd700a1cacd
abhilash-kundoor <91517464+abhilash-kundoor@users.noreply.github.com>
2021-11-08 19:37:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Adding the required libraries
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 const {check, validationResult, body} = require('express-validator');
 
@@ -211,6 +211,6 @@ app.post('/process',[
     }
 });
 // Listening on port 80
-app.listen(80);
+app.listen(3000);
 
 console.log('Everything executed fine.. website at port 80....');
